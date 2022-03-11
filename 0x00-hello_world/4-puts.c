83a60ade151c95e0e6f6cb65ee6fc0eb0f71eb93
ifemi27 <opatolaifeoluwav@gmail.com>
2022-03-10 22:52:15 -0800

    update to task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
@@ -9,3 +9,6 @@ int main(void)
   puts("\'Programming is like building a multilingual puzzle");
   return (0);
 } 
+
+
+
