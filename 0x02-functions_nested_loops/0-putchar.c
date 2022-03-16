0571e0dae60f8f0905123a6839ec3d3075e800b2
ifemi27 <opatolaifeoluwav@gmail.com>
2022-03-16 05:48:05 -0700

    nested commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
- * main - print_putchar followed by a new line
+ * main - prints_putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char *ch = "_putchar";
 
-	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
-	_putchar('\n);
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,13 @@
-#include <unistd.h>
-#ifndef MAIN.H
-#define MAIN.H
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is required, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-#endif
+int_putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int_islower(int c);
+int_isalpha(int c);
+int print_sign(int n);
+int_abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to =_98(int n);
+void print_times_table(int n);
