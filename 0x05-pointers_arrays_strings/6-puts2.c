7499bd7cb64dda3c3d17c869f5183f857c5e46ea
ifemi27 <opatolaifeoluwav@gmail.com>
2022-03-21 08:16:50 -0700

    puts2 commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,40 +1,39 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
- *
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
  **/
-
 
 void puts2(char *str)
 
 {
 
-	int i, j = 0;
+	int count = 0;
 
 
 
-	while (str[i] != '\0')
-
-	{
-
-		i++;
-
-	}
-
-
-
-	while (j < i)
+	while (count >= 0)
 
 	{
 
-		_putchar(str[j]);
+		if (str[count] == '\0')
 
-		j = j + 2;
+		{
+
+			_putchar('\n');
+
+			break;
+
+		}
+
+		if (count % 2 == 0)
+
+			_putchar(str[count]);
+
+		count++;
 
 	}
-
-	_putchar('\n');
 
 }
